fccec45d6aabe2f88283931ef15f6c6f8f7466f3
Nicholas Ormrod <nicholas.ormrod+github@gmail.com>
2025-10-27 12:00:53 -0700

    planet functions

--- state.py
+++ state.py
@@ -24,6 +24,22 @@ class Planet:
     colonization_shipping_level: int = 1
     colonization_cargo_level: int = 1
 
+    def output(self):
+        pass
+
+    def mining_speed(self) -> float:
+        level = self.mining_level
+        return 0.25 + 0.1 * (level - 1) + 0.017 * (level - 1) ** 2
+
+    def ship_speed(self) -> float:
+        level = self.shipping_level
+        return 1 + 0.2 * (level - 1) + (1 / 75) * (level - 1) ** 2
+
+    def ship_capacity(self) -> int:
+        level = self.cargo_level
+        result = 5 + 2 * (level - 1) + 0.1 * (level - 1) ** 2
+        return round(result)
+
 
 @dataclass
 class Ore:
